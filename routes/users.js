987531fb75208471ca44ea812a5d563a38f86339
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-06-27 18:25:40 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// noinspection JSUnusedLocalSymbols
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
